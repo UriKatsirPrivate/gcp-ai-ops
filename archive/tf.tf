b7efc8fc4952de6cd40c2a113ae1984a711b5935
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-10-10 18:31:22 +0300

    added sample

--- archive/tf.tf
+++ archive/tf.tf
@@ -8,3 +8,15 @@ resource "google_compute_firewall" "allow_all" {
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["allow-all"]
 }
+
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.default.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["77.125.87.177/32"]
+  target_tags = ["allow-ssh"]
+}
